1c440a1c1ab58de41c430546e929341fac5fc9eb
zeel <zeel02@gmail.com>
2021-03-02 12:00:12 -0500

    Created mockup in Vue

---
+++ layouts/adventure.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <header class="site-header">
+      <figure class="logo">
+        <NuxtLink class="brand" :to="{name: 'index'}">Adventure Library</NuxtLink>
+      </figure>
+      <div class="searchbar">
+        <input type="text" name="serach">
+      </div>
+      <div class="account-actions">
+        <a class="logout-button">Logout</a>
+        <figure class="member-avatar"></figure>
+        <a class="account-link">My Account</a>
+      </div>
+      <nav class="main-navigation">
+        <ul>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Purchase Coins</a></li>
+          <li><NuxtLink to="/mockups/filler">Filler</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <header class="page-header">
+        <h1>Page Title</h1>
+        <h2>Page call to action / subtitle.</h2>
+      </header>
+      <main class="page-body">
+        <Nuxt />
+      </main>
+    </main>
+    <footer>
+      <h1>Adventure Library</h1>
+      <p>Copyright text</p>
+      <nav class="footer-links">
+        <ul>
+          <li><a href="">Link 1</a></li>
+          <li><a href="">Link 2</a></li>
+          <li><a href="">Link 3</a></li>
+          <li><a href="">Link 4</a></li>
+          <li><a href="">Link 5</a></li>
+        </ul>
+      </nav>
+      <section class="social-media">
+        <ul>
+          <li><a href=""><img src="" alt="Facebook"></a></li>
+          <li><a href=""><img src="" alt="Twitter"></a></li>
+          <li><a href=""><img src="" alt="Instagram"></a></li>
+        </ul>
+      </section>
+    </footer>
+  </main>
+</template>
---
+++ pages/mockups/filler.vue
@@ -0,0 +1,17 @@
+
+<template>
+	<article>
+		<p>Ex non ex duis. Enim dolore officia duis minim anim commodo et eu do non Lorem. Nisi laborum et et cillum cupidatat anim laboris. Esse quis cupidatat nostrud et consequat incididunt do veniam consectetur reprehenderit aute. Et commodo tempor aliquip ea voluptate tempor irure. Ipsum adipisicing elit aute officia proident voluptate.</p>
+		<p>Aliquip exercitation et velit in veniam velit eiusmod deserunt. Aliqua ullamco nisi voluptate qui ad. Aute nisi adipisicing eu reprehenderit sint ex. Incididunt mollit qui magna irure culpa.</p>
+		<p>Sint enim id magna exercitation nisi elit. Amet ea veniam excepteur Lorem sunt anim incididunt tempor cillum aute ad cupidatat proident in et. Do et do eu proident. Occaecat proident ullamco est cillum ea sunt quis veniam dolore aliqua cillum Lorem. Exercitation exercitation officia cupidatat. Ad cupidatat culpa laborum proident eu nisi ipsum est. Aute proident in cillum aliquip enim qui irure dolore aute. Nulla esse sint aliqua ipsum aliqua dolor consectetur pariatur officia non adipisicing sunt voluptate enim adipisicing.</p>
+		<p>In ipsum voluptate nostrud veniam. Voluptate non veniam dolore incididunt amet velit. Ipsum et ex esse pariatur ea amet aute ex excepteur nostrud esse velit duis Lorem. Non eiusmod id enim eu pariatur. Do quis irure nulla ex id aute amet occaecat excepteur officia cupidatat laborum minim. Enim cillum anim consequat excepteur cillum. Incididunt officia deserunt ad sit nostrud aute eu enim exercitation excepteur. Ex commodo sit cillum officia nulla et.</p>
+		<p>Ex consectetur quis sint officia dolore laborum dolore dolor qui in enim nostrud proident quis sunt. Tempor fugiat deserunt sunt commodo esse voluptate nulla id proident eu minim pariatur commodo. Nulla proident laborum dolore nisi duis cillum culpa mollit anim deserunt exercitation velit. Tempor ea et velit dolore incididunt elit amet velit aliquip ut commodo tempor.</p>
+		<p>Ut irure labore officia commodo nulla. Consectetur ut ullamco incididunt cupidatat voluptate velit irure laboris consequat. Aliquip tempor incididunt nulla officia non incididunt Lorem aliqua reprehenderit nisi non ut dolore occaecat. Elit amet laborum anim. Lorem sint officia voluptate sunt sit sunt nisi elit adipisicing in.</p>
+	</article>
+</template>
+<script>
+  export default {
+    layout: 'adventure'
+    // page component definitions
+  }
+</script>
