36b17fa037fb34c98d5f94a93f2bd57e6dc6c9d4
Vindexus <vindexus@gmail.com>
2021-04-29 15:48:52 -0700

    Form mixin to tab

--- mixins/Forms.vue
+++ mixins/Forms.vue
@@ -18,50 +18,53 @@ export const newForm = (data = {}) : Form => {
 }
 
 @Component({
-  components: {
-    FormErrors: FormErrors
-  }
+	components: {
+		FormErrors: FormErrors
+	}
 })
 export default class FormMixin extends Vue {
-  public form = newForm()
-  public formSuccessMessage = 'Form submitted'
-  validateForm () : string {
-    return ''
-  }
+	public form = newForm()
+	public formSuccessMessage = ''
+	validateForm () : string {
+		return ''
+	}
 
-  // This should be overwritten by any component
-  // that implements this mixin
-  async formAction () {
+	// This should be overwritten by any component
+	// that implements this mixin
+	async formAction () {
 		throw new Error('Form action needs to be implemented by the inheriting component')
-  }
+	}
 
-  async formSuccess () {
-    this.notifySuccess(this.formSuccessMessage)
-  }
+	async formSuccess () {
+		if (!this.formSuccessMessage) {
+			return
+		}
+		this.notifySuccess(this.formSuccessMessage)
+	}
 
-  async submit (e: any) {
-    e.preventDefault()
-    const err = this.validateForm()
-    if (err && err.length) {
-      this.form.error = err
-      this.form.submitting = false
+	async submit (e: any) {
+		e.preventDefault()
+		const err = this.validateForm()
+		if (err && err.length) {
+			this.form.error = err
+			this.form.submitting = false
 			console.log('error', err)
 			return
-    }
-    this.form.submitting = true
-    this.form.error = ''
-    try {
+		}
+		this.form.submitting = true
+		this.form.error = ''
+		try {
 			console.log('doing the form action', this.formAction)
-      await this.formAction()
+			await this.formAction()
 			console.log('done the form action')
-      this.form.submitting = false
-      await this.formSuccess()
-    } catch (ex) {
+			this.form.submitting = false
+			await this.formSuccess()
+		} catch (ex) {
 			console.log('caught this', ex)
-      this.form.error = ex.toString()
-      this.form.submitting = false
-      //this.notifyError('Error submitting form:' + ex.toString())
-    }
-  }
+			this.form.error = ex.toString()
+			this.form.submitting = false
+			//this.notifyError('Error submitting form:' + ex.toString())
+		}
+	}
 }
 </script>
