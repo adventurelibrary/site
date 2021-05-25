fe7396f5f68ee20cf82f478125106745b462c59b
zeel <zeel02@gmail.com>
2021-05-24 23:43:23 -0400

    Initial user page styles

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,23 +1,23 @@
 <template>
 	<article class="user-profile">
 		<section class="profile-details">
-			<img class="profile-image">
+			<img src="~/assets/coolicons/svg/user/user.svg" class="profile-image">
 			<h1 class="username">{{user.username}}</h1>
 			<ul class="user-meta">
-				<li class="join-date">{{user.joinDate}}</li>
-				<li class="status">{{user.status}}</li>
-				<li class="asset-count">{{user.assetCount}}</li>
+				<li class="join-date">Date Joined: {{user.joinDate}}</li>
+				<li class="status">Status: {{user.status}}</li>
+				<li class="asset-count">Assets: {{user.assetCount}}</li>
 			</ul>
 		</section>
 		<nav class="tabs">
-			<a href="" class="tab-control">Assets & Bundles</a>
+			<a href="" class="tab-control active">Assets & Bundles</a>
 			<a href="" class="tab-control">History</a>
 			<a href="" class="tab-control">Settings</a>
 			<a href="" class="button upgrade">Become An Artist</a>
 		</nav>
-		<section class="tab inventory">
+		<section class="tab inventory active">
 			<div class="view-toggle">
-				<a href="" class="toggle">My Assets</a>
+				<a href="" class="toggle active">My Assets</a>
 				<a href="" class="toggle">My Bundles</a>
 			</div>
 			
@@ -44,11 +44,14 @@ import {User} from "~/lib/users/user-types"
 })
 export default class UserProfile extends Vue {
 	//@State('user') user : User
-	user : User = {
+	user : any = {
 		id: "id",
 		admin: true,
 		email: "contact@adventurelibrary.art",
-		username: "Some User"
+		username: "Name of User",
+		joinDate: "2021",
+		status: "User",
+		assetCount: "15"
 	}
 }
 </script>
