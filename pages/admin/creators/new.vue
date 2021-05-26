6d3c1cf484200c648e0992945f7af60a3fcdb8f5
Vindexus <vindexus@gmail.com>
2021-05-25 17:22:55 -0700

    Page for adding a new creator

---
+++ pages/admin/creators/new.vue
@@ -0,0 +1,95 @@
+<template>
+	<CCard>
+		<CCardHeader>
+			Add a Creator
+		</CCardHeader>
+		<CCardBody>
+			<form @submit="submit">
+				<FormErrors :error="form.error" />
+				<CreatorFields v-model="form.data" />
+				<div>
+					<CButton type="submit" color="primary">Submit</CButton>
+				</div>
+			</form>
+		</CCardBody>
+	</CCard>
+</template>
+<script lang="ts">
+import {Component, mixins} from "nuxt-property-decorator";
+import {Context} from "@nuxt/types";
+import AdminPage from "~/admin/admin-page";
+import {Creator} from "~/modules/creators/creator-types";
+import {
+	addCreator,
+	creatorToFormData, getCreatorById,
+	newCreatorAjax, saveCreator,
+} from "~/modules/creators/creator-api";
+import LoadingContainer from "~/components/LoadingContainer.vue";
+import CreatorFields from "~/modules/creators/components/CreatorFields.vue";
+import FormMixin from "~/mixins/Forms.vue";
+
+
+@Component({
+	components: {
+		LoadingContainer: LoadingContainer,
+		CreatorFields: CreatorFields
+	}
+})
+export default class NewCreatorPage extends mixins(AdminPage, FormMixin) {
+
+	head () {
+		return {
+			title: 'Add a Creator',
+		}
+	}
+
+	validateForm(): string {
+		if (!this.form.data.name) {
+			return 'Name is required'
+		}
+		return ''
+	}
+
+	async formAction () {
+		const id = await addCreator(this.form.data)
+		this.notifySuccess('Creator added')
+		this.$router.push({
+			name: 'admin-creators-id',
+			params: {
+				id: id
+			}
+		})
+	}
+
+	async asyncData (ctx: Context) {
+		const creatorAjax = newCreatorAjax()
+		let data : any = {}
+		try {
+			const creator = await getCreatorById(ctx.route.params.id)
+			console.log('gotted', creator)
+			creatorAjax.data = creator
+			data = creatorToFormData(creator)
+		} catch (ex) {
+			creatorAjax.error = ex
+		}
+		creatorAjax.loading = false
+		console.log('data', data)
+		return {
+			creatorAjax: creatorAjax,
+			data: data,
+			form: {
+				data
+			},
+			id: ctx.params.id
+		}
+	}
+
+	deleteCreator () {
+		if (prompt('Type DELETE to delete creator') !== 'DELETE') {
+			return
+		}
+
+		this.notifySuccess('Creator deleted (but not really yet)')
+	}
+}
+</script>
