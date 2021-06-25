868a4e2ae3a91d9b4c5c523d7da1e49042681905
Colin Kierans <vindexus@gmail.com>
2021-06-24 18:33:36 -0700

    Add google form link to Become an Artist

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -2,7 +2,7 @@
 	<ProfilePage active-tab="settings">
 		settings here I guess
 		<Fragment slot="actions">
-			<a v-if="!isCreator" @click="becomeAnArtist" class="button upgrade">Become An Artist</a>
+			<a v-if="!isCreator" href="https://docs.google.com/forms/d/1e_7ori6eqo7OczyuqvLpFsBSY-Ke3aNwEiVztTVygvY/viewform" target="_blank" class="button upgrade">Become An Artist</a>
 		</Fragment>
 	</ProfilePage>
 </template>
