77fa1ba3c47447d3383895a58a8b521ab139f5c3
Colin Kierans <vindexus@gmail.com>
2021-07-11 17:33:13 -0700

    Updates to conditional rendering in loading container.

--- components/LoadingContainer.vue
+++ components/LoadingContainer.vue
@@ -1,17 +1,17 @@
 <template>
 	<Fragment role="loading-container" class="loading-container">
-		<Fragment v-if="loading">
+		<template v-if="loading">
 			<slot name="loading">
 				<div class="loading-message">Loading...</div>
 			</slot>
-		</Fragment>
-		<Fragment v-if="error">
+		</template>
+		<template v-if="error">
 			<slot name="error">
 				<div class="error">
 					{{error}}
 				</div>
 			</slot>
-		</Fragment>
+		</template>
 		<template v-if="!loading && !error">
 			<slot />
 		</template>
