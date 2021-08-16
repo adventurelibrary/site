56db39a77fd49f10a0187edca44e2318bdc50c35
Styrmir Thorarinsson <StyrmirThorarins@users.noreply.github.com>
2021-08-16 16:35:45 +0000

    Update tags-api.ts

--- modules/tags/tags-api.ts
+++ modules/tags/tags-api.ts
@@ -1,7 +1,7 @@
 import {AssetTag} from "~/modules/assets/asset-types";
 import {ASSET_TAGS} from "~/modules/tags/tags-consts";
 
-export function getTagById(id: string) : AssetTag | undefined {	
+export function getTagById(id: string) : AssetTag | undefined {
 	return ASSET_TAGS.find(at => at.id === id.toLowerCase())
 }
 
