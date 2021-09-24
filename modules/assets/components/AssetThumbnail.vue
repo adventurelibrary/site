e468f2334bfb83948090a347323fe62a988eee38
Vindexus <vindexus@gmail.com>
2021-09-24 08:48:20 -0700

    Added lazy loading and fadein to asset thumbnails.

--- modules/assets/components/AssetThumbnail.vue
+++ modules/assets/components/AssetThumbnail.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="asset-thumbnail thumbnail" :style="`background-image: url(${asset.thumbnail});`">
+	<div class="asset-thumbnail thumbnail" :style="`background-image: url(${src}); opacity: ${opacity}`">
 		<!-- This Element Intentionally Left Empty -->
 	</div>
 </template>
@@ -10,6 +10,55 @@ import {Asset} from "~/modules/assets/asset-types";
 
 @Component
 export default class AssetThumbnail extends Vue {
+	loaded = false
+	loading = false
+	src = ''
 	@Prop() asset : Asset
+
+	mounted () {
+		if (process.client) {
+			window.addEventListener('scroll', this.onScroll)
+			this.onScroll()
+		} else {
+			this.src = this.asset.thumbnail
+			this.loaded = true
+		}
+	}
+
+	destroyed () {
+		window.removeEventListener('scroll', this.onScroll)
+	}
+
+	// When the user scrolls, or when
+	onScroll () {
+		const rect = this.$el.getBoundingClientRect()
+		const cutoff = document.documentElement.clientHeight + 100
+		console.log('cutoff', cutoff)
+		console.log('rect', rect)
+		if (rect.top <= cutoff) {
+			console.log('cutoff yes', this.asset.name)
+			this.loadImage()
+			window.removeEventListener('scroll', this.onScroll)
+		}
+	}
+
+	loadImage () {
+		// This is to prevent one scroll creating a bunch of images
+		if (this.loading) {
+			return
+		}
+		this.loading = true
+		this.src = this.asset.thumbnail
+		let img = document.createElement('img')
+		img.onload = () => {
+			this.loaded = true
+			img = null
+		}
+		img.setAttribute('src', this.src)
+	}
+
+	get opacity () {
+		return this.loaded ? 1 : 0
+	}
 }
 </script>
